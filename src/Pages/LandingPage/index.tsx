b6902a1578d1c5099cf6eb0f72eeec7bbb276ef4
Jorge Muladi <jorgeosattimuladi@gmail.com>
2024-10-03 09:10:49 +0700

    feat: added About and Footer in Landing Page

--- src/Pages/LandingPage/index.tsx
+++ src/Pages/LandingPage/index.tsx
@@ -1,16 +1,20 @@
-import {FC} from "react";
+import { FC } from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Portofolio from "../../components/Portofolio";
+import About from "../../Pages/LandingPage/Components/About";
+import Footer from "../../Pages/LandingPage/Components/Footer";
 
 const LandingPage: FC = () => {
   return (
     <>
       <Header />
       <Hero />
+      <About />
       <Portofolio />
+      <Footer />
     </>
   );
-}
+};
 
 export default LandingPage;
